eb07156d994b7dabf4e94d9359f97233c9c7eb44
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-15 03:52:07 +0100

    style: define chat layout

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -6,16 +6,46 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://www.rollingstone.com/wp-content/uploads/2019/11/CamilaCabello.jpg?w=1581&h=1054&crop=1'),
+        appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.rollingstone.com/wp-content/uploads/2019/11/CamilaCabello.jpg?w=1581&h=1054&crop=1'),
+            ),
           ),
+          title: const Text('My Love  🥰'),
+          centerTitle: false,
         ),
-        title: const Text('My Love  🥰'),
-        centerTitle: false,
+        body: const _ChatView());
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 40,
+              itemBuilder: (context, index) {
+                return Text('Item $index');
+              },
+            )),
+            const Text(
+              'Chat',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
