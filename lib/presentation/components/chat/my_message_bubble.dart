3d79934fa988674178f2de1aaf06c0e954a9f599
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-15 04:41:22 +0100

    style(Chat):creation MyMessageBubble component

---
+++ lib/presentation/components/chat/my_message_bubble.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyMessageBubble extends StatelessWidget {
+  const MyMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.primary, borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'lorem sit amet, consectetur',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 10)
+      ],
+    );
+  }
+}
