18b6ea1009c8823813c7b5822b7e9b0f6f95d32e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-16 12:28:44 +0100

    feat: implementation reply hers method

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:maybe_app/config/helpers/get_no_abswer.dart';
 import 'package:maybe_app/domain/entities/message_entity.dart';
 
 class ChatProvider extends ChangeNotifier {
   final controller = ScrollController();
+  final getYesNoAnser = GetYesNoAnswer();
+
   List<MessageEntity> messageList = [
     MessageEntity(
       text: 'hi Camilla',
@@ -23,11 +26,23 @@ class ChatProvider extends ChangeNotifier {
 
     messageList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      await herReply();
+    }
+
     notifyListeners();
     moveScrollToBottom();
   }
 
-  void moveScrollToBottom() {
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnser.getAnswer();
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
     controller.animateTo(
       controller.position.maxScrollExtent,
       duration: const Duration(milliseconds: 300),
